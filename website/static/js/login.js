2b9b6a0d853af8077589c03f053560ff840fa87d
rdb323 <barthr@seattleu.edu>
2020-03-06 12:16:31 -0800

    added the login html, css, and js

---
+++ website/static/js/login.js
@@ -0,0 +1,9 @@
+
+
+function submission(){
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+
+    console.log("Username submitted = " + user);
+    console.log("Password submitted = " + pass);
+}
